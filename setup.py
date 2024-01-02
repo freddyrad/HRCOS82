2b410c32aac23bb9ed7acc8f766f150ee118ac28
Freddy <Freddyr@acs.altech.co.za>
2024-01-02 16:54:18 +0200

    Added Distribution build process

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='ResearchArtifact34599304',
-    version='1',
-    packages=[''],
+    version='1.1',
+    packages=[],
     url='',
     license='',
     author='Freddy Rademeyer',
